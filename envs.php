125b75d197b6cd28280c9179803fd15c390457fe
Alfredo <21030761@itcelaya.edu.mx>
2024-06-16 23:13:56 -0600

    EL puto archivo envs cambiado

---
+++ envs.php
@@ -0,0 +1,7 @@
+<?php 
+
+    putenv('DB_PASSWORD=dqeI1NuqRL5fxw03rK7eJy9H04hGuxGu');  
+    putenv('DB_USER=admin');
+    putenv('DB_NAME=asesorias_db');
+    putenv('DB_HOST=dpg-cpnrf1uehbks738cfd90-a.oregon-postgres.render.com');
+?>
