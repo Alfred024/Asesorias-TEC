e1e25f5cd3944730a51ec646f4486d3828242124
Alfredo <21030761@itcelaya.edu.mx>
2024-06-20 01:50:07 -0600

    Excel

--- classes/class_office.php
+++ classes/class_office.php
@@ -4,13 +4,19 @@
 if (!isset($_SESSION)) session_start();
 if (!class_exists("Class_Database")) include "../classes/class_database.php";
 
-include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/IOFactory.php';
-include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Spreadsheet.php';
-include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Shared/File.php';
-include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/Xlsx';
+// include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/IOFactory.php';
+// include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Spreadsheet.php';
+// include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Shared/File.php';
+// include '../resources/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Reader/Xlsx';
+// use PhpOffice\PhpSpreadsheet\Spreadsheet;
+// use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+// use PhpOffice\PhpSpreadsheet\IOFactory;
+
+require '../vendor/autoload.php';
+//require __DIR__ .  '/vendor/autoload.php';
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class Excel extends Class_Database{
     
@@ -25,33 +31,33 @@ class Excel extends Class_Database{
                     
                     $inputFileNamePath = $_FILES['students_excel_file']['tmp_name'];
                     echo $inputFileNamePath;
-                    $spreadsheet = PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileNamePath);
-                    // $data = $spreadsheet->getActiveSheet()->toArray();
+                    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileNamePath);
+                    $data = $spreadsheet->getActiveSheet()->toArray();
 
-                    // $count = "0";
-                    // foreach ($data as $row) {
-                    //     if ($count > 0) {
-                    //         $user = $row['0'];
-                    //         $email = $row['1'];
-                    //         $names = $row['2'];
-                    //         $first_last_name = $row['3'];
-                    //         $second_last_name = $row['4'];
+                    $count = "0";
+                    foreach ($data as $row) {
+                        if ($count > 0) {
+                            $user = $row['0'];
+                            $email = $row['1'];
+                            $names = $row['2'];
+                            $first_last_name = $row['3'];
+                            $second_last_name = $row['4'];
 
-                    //         $studentQuery = 
-                    //         "insert into usuario (3, usuario, email, nombres, apellido_paterno, apellido_materno)
-                    //         values (3, ".$user.", ".$email.", ".$names.", ".$first_last_name.", ".$second_last_name.");";
-                    //         $this->query($studentQuery);
-                    //         $msg = true;
-                    //     } else {
-                    //         $count = "1";
-                    //     }
-                    // }
+                            $studentQuery = 
+                            "insert into usuario (3, usuario, email, nombres, apellido_paterno, apellido_materno)
+                            values (3, ".$user.", ".$email.", ".$names.", ".$first_last_name.", ".$second_last_name.");";
+                            $this->query($studentQuery);
+                            $msg = true;
+                        } else {
+                            $count = "1";
+                        }
+                    }
 
-                    // if (isset($msg)) {
-                    //     // Importación exitosa
-                    // } else {
-                    //     // Importación NO exitosa
-                    // }
+                    if (isset($msg)) {
+                        // Importación exitosa
+                    } else {
+                        // Importación NO exitosa
+                    }
                 } else {
                     echo('No se cargó ningún archivo');
                 }
